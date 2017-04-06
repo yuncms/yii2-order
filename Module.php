c8a296b95e9ef4114cce9f268c59ca88948bb675
xuchao <85825770@qq.com>
2017-04-06 12:33:21 +0800

    init

---
+++ Module.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\order;
+
+use Yii;
+
+/**
+ * This is the main module class for the yii2-order.
+ */
+class Module extends \yii\base\Module
+{
+
+}
